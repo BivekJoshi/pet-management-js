97932bec4386361f8f2f3d7ab63dcd2c4edd481e
bivek.joshi <bivek.joshi@dghub.io>
2024-02-13 12:22:46 +0545

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '',
+  server: {
+    port: 5173,
+  },
+});
